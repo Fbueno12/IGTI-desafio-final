bb34ce35f2c9bfba5affffb39a0e061155c71e2f
fbueno12 <felipebueno181@gmail.com>
2020-09-05 19:30:05 -0300

    creating base project to start developing

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './global.scss';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
